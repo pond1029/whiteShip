aeebb0c2843311b243064e165df756200b42d99e
"LeeJaeYeon" <"LeeJaeYeon"@host.docker.internal>
2020-12-02 00:25:06 +0900

    first commit

---
+++ white_ship/src/test/java/com/pond/ws/WhiteShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pond.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
